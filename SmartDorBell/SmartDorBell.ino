dc867ed8d7b7f4e90b9d7f9c8ef0bf1ac57afa8e
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-05-06 09:51:08 +0200

    feat(): Buzzer and LED Screen

--- SmartDorBell/SmartDorBell.ino
+++ SmartDorBell/SmartDorBell.ino
@@ -19,13 +19,17 @@
 #include <Adafruit_Fingerprint.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Button.h>
+#include <Buzzer.h>
 
 #if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
 // For UNO and others without hardware serial, we must use software serial...
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
 // Set up the serial port to use softwareserial..
-SoftwareSerial mySerial(2, 3);
+SoftwareSerial mySerial(10, 11);
+Button buttonBell(13); // Connect your button between pin 2 and GND
+Buzzer buzzer(12);
 
 #else
 // On Leonardo/M0/etc, others with hardware serial, use hardware serial!
@@ -44,9 +48,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 void setup()
 {
   Serial.begin(9600);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  buttonBell.begin();
 
   while (!Serial);  // For Yun/Leo/Micro/Zero/...
   delay(100);
@@ -75,55 +77,13 @@ void setup()
   finger.getTemplateCount();
   
   lcd.init();// initialize the lcd 
-
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("1: Login");
-  lcd.setCursor(0, 1);
-  lcd.print("2: Sign Up");
-
-  Serial.println("Wähle eine Option:");
-  Serial.println("1 - Einloggen");
-  Serial.println("2 - Finger registrieren");
-
-   // Warten auf Nutzereingabe
-  while (userChoice == 0) {
-    if (Serial.available()) {
-      char input = Serial.read();
-      if (input == '1') {
-        userChoice = 1;
-      } else if (input == '2') {
-        userChoice = 2;
-      }
-    }
-  }
-
   lcd.clear();
-
-  if (userChoice == 1) {
-    lcd.setCursor(4, 0);
-    lcd.print("Welcome");
-    lcd.setCursor(0, 1);
-    lcd.print("Authenticating...");
-
-
-    if (finger.templateCount == 0) {
-      Serial.print("Sensor doesn't contain any fingerprint data.");
-      userChoice = 2;
-    }
-  }
-  else {
-    Serial.println("Waiting for valid finger...");
-    Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
-  }
-
-    
-  if (userChoice == 2) {
-    lcd.setCursor(0, 0);
-    lcd.print("register");
-    lcd.setCursor(0, 1);
-    lcd.print("Please wait...");
-  }
+  lcd.setCursor(0, 0);
+  lcd.print("Waiting for");
+  lcd.setCursor(0, 1);
+  lcd.print("valid finger");
+  
 }
 
 uint8_t readnumber(void) {
@@ -136,12 +96,109 @@ uint8_t readnumber(void) {
   return num;
 }
 
-void loop()                     // run over and over again
+void loop() // run over and over again
 {
-  if (userChoice == 1) { 
-    getFingerprintID();
+  if (digitalRead(13) == HIGH) {
+    lcd.clear();
+    lcd.setCursor(2, 0);
+    lcd.print("Bell pressed");
+
+    buzzer.begin(100);
+
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_C7, 80);
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_G7, 80);
+    buzzer.sound(0, 240);
+    buzzer.sound(NOTE_G6, 80);
+    buzzer.sound(0, 240);
+    buzzer.sound(NOTE_C7, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_G6, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_E6, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_A6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_B6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_AS6, 80);
+    buzzer.sound(NOTE_A6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_G6, 100);
+    buzzer.sound(NOTE_E7, 100);
+    buzzer.sound(NOTE_G7, 100);
+    buzzer.sound(NOTE_A7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_F7, 80);
+    buzzer.sound(NOTE_G7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_C7, 80);
+    buzzer.sound(NOTE_D7, 80);
+    buzzer.sound(NOTE_B6, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_C7, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_G6, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_E6, 80);
+    buzzer.sound(0, 160);
+    buzzer.sound(NOTE_A6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_B6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_AS6, 80);
+    buzzer.sound(NOTE_A6, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_G6, 100);
+    buzzer.sound(NOTE_E7, 100);
+    buzzer.sound(NOTE_G7, 100);
+    buzzer.sound(NOTE_A7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_F7, 80);
+    buzzer.sound(NOTE_G7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_E7, 80);
+    buzzer.sound(0, 80);
+    buzzer.sound(NOTE_C7, 80);
+    buzzer.sound(NOTE_D7, 80);
+    buzzer.sound(NOTE_B6, 80);
+    buzzer.sound(0, 160);
+
+    buzzer.end(2000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Waiting for");
+    lcd.setCursor(0, 1);
+    lcd.print("valid finger");
+  }	
+  uint8_t result = getFingerprintID();
+  if (result == finger.fingerID) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    Serial.print("access allowd");
+    lcd.print("Access allowed");
+    delay(2000);
   }
-  if (userChoice == 2) {
+  else if (result == FINGERPRINT_NOTFOUND) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Access Denied");
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Waiting for");
+    lcd.setCursor(0, 1);
+    lcd.print("valid finger");
+  }
+  /*if (userChoice == 2) {
     Serial.println("Ready to enroll a fingerprint!");
     Serial.println("Please type in the ID # (from 1 to 127) you want to save this finger as...");
     id = readnumber();
@@ -153,6 +210,7 @@ void loop()                     // run over and over again
 
     while (! getFingerprintEnroll() );
   }
+  */
 }
 
 uint8_t getFingerprintEnroll() {
@@ -344,32 +402,11 @@ uint8_t getFingerprintID() {
   p = finger.fingerSearch();
   if (p == FINGERPRINT_OK) {
     Serial.println("Found a print match!");
-    lcd.clear();
-    lcd.setCursor(2, 0);
-    lcd.print("Access allowed");
-
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
     Serial.println("Communication error");
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Communication");
-    lcd.setCursor(1, 0);
-    lcd.print("error");
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
     Serial.println("Did not find a match");
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("access denied");
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
     return p;
   } else {
     Serial.println("Unknown error");
